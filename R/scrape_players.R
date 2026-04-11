4c57c13234afebefa6fc0bff85ab0ddf0bf261f0
Joseph Hefner <Hefnerjoseph87@gmail.com>
2026-04-11 08:34:41 -0700

    Change free agent source from Spotrac to OTC

--- R/scrape_players.R
+++ R/scrape_players.R
@@ -4,86 +4,58 @@ library(rvest)
 library(nflreadr)
 library(janitor)
 library(stringr)
+library(httr)
 
 # dataset -----------------------------------------------------------------
 
-load_spotrac_free_agents <- function(){
-  url <- "https://www.spotrac.com/nfl/free-agents/"
+scrape_otc_free_agents <- function(year = nflreadr::get_current_season(roster = TRUE)){
   
-  spotrack_raw <- httr::GET(
-    'https://www.spotrac.com/nfl/free-agents/_/year/2024/status/available/sort/contract_value',
-    httr::add_headers(`user-agent` = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0")
+  res <- POST(
+    "https://overthecap.com/wp-admin/admin-ajax.php",
+    body = list(
+      action = "get_free_agents",
+      season = year
+    ),
+    encode = "form"
   )
   
-  players <- read_html(spotrack_raw) |> 
-    html_elements(".player") |> 
-    html_text()
+  html <- content(res, as = "text")
   
-  position <- read_html(spotrack_raw) |> 
-    html_elements(".player+ .text-center") |> 
-    html_text()
+  html_wrapped <- paste0(
+    "<table><tbody>",
+    html,
+    "</tbody></table>"
+  )
   
-  free_agents1<- tibble::tibble(
-    player = str_trim(str_replace(players,',','')), 
-    position = str_trim(position), 
-  ) |> 
-    mutate(rank = row_number(), .before = player)
-    # separate_wider_delim(player, 
-    #                      delim = " ", 
-    #                      names = c("old", "new"), 
-    #                      too_many = "merge") |> 
-    # mutate(new = str_trim(str_replace(new,',',''))) |> 
-    # # Andrew Van Ginkel's name shows up weird, thus we need to fix it. 
-    # mutate(new = case_when(
-    #   str_detect(new, "Andrew Van Ginkel") ~ "Andrew Van Ginkel", 
-    #   TRUE ~ new
-    # ))
+  otc_fa <- read_html(html_wrapped) |>
+    html_element("table") |>
+    html_table() |> 
+    rename(player = X1, position_roster = X2, old_team = X3, new_team = X4) |> 
+    mutate(new_team = na_if(trimws(new_team), "")) |> 
+    filter(is.na(new_team))
   
-  free_agents_good <- free_agents1 |> 
-    filter(str_starts(player, "Jr.", negate = TRUE),  
-           str_starts(player, "II", negate = TRUE),   
-           str_starts(player, "III", negate = TRUE),  
-           str_starts(player, "IV", negate = TRUE))
-  
-  free_agents_problem <- free_agents1 |> 
-    filter(str_starts(player, "Jr.") | 
-             str_starts(player, "II") |  
-             str_starts(player, "III") | 
-             str_starts(player, "IV")) |> 
-    separate_wider_delim(player, 
-                         delim = " ", 
-                         names = c("old2", "player"), 
-                         too_many = "merge") |> 
+  free_agents <- otc_fa |> 
     mutate(player = str_trim(str_replace(player,',',''))) |> 
-    select(-old2)
-  
-  
-  
-  free_agents <- bind_rows(free_agents_good, free_agents_problem) |> 
+    mutate(position_roster = str_trim(position_roster)) |> 
+    mutate(rank = row_number(), .before = player) |> 
     arrange(rank) |> 
-    mutate(name = nflreadr::clean_player_names(player), .after = "player") |> 
-    select(-player) |> 
+    mutate(player = nflreadr::clean_player_names(player), .after = "player") |> 
     mutate(position_roster = case_when(
-      position %in% c("LT", "RT", "T") ~ "OT", 
-      position %in% c("LG", "RG", "G") ~ "OG", 
-      position %in% c("FB") ~ "RB", 
-      position %in% c("DE", "OLB") ~ "EDGE", 
-      position %in% c("ILB") ~ "LB", 
-      position %in% c("LDT", "RDT", "NT", "DT") ~ "DL", 
-      position %in% c("FS", "SS") ~ "S", 
-      TRUE ~ position
-    ), .after = position) |> 
-    select(player = name, position = position_roster)
-
+      position_roster %in% c("LT", "RT", "T") ~ "OT", 
+      position_roster %in% c("LG", "RG", "G") ~ "OG", 
+      position_roster %in% c("FB") ~ "RB", 
+      position_roster %in% c("DE", "OLB") ~ "EDGE", 
+      position_roster %in% c("ILB") ~ "LB", 
+      position_roster %in% c("LDT", "RDT", "NT", "DT", "IDL") ~ "DL", 
+      position_roster %in% c("FS", "SS") ~ "S", 
+      TRUE ~ position_roster
+    ), .after = position_roster) |> 
+    select(player, position_roster)
+  
+  return(free_agents)
 }
 
-free_agents <- load_spotrac_free_agents()
+free_agents <- scrape_otc_free_agents()
 
 saveRDS(free_agents, paste0("Data/", "new_players.rds"))
-
-
-
-
-
-
 
